b9be42d80c834afa9f7b05a3a5d87a65fc772bb0
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-05-24 11:38:44 +0900

    新規登録画面の実装
    バリデーションの設定とエラーメッセージの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      #本来は読書記録一覧へリダイレクトさせるが、暫定的にユーザー詳細画面へリダイレクト
+      redirect_to @user, notice: "ユーザー「#{@user.name}」を登録しました。" 
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,5 +23,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
