21f92cb7c50d674a792da886b03d2cf65ed3c8f4
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-05-26 22:09:04 +0900

    ログイン機能の追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+    @user = User.find_by(email:session_params[:email])
+
+    if @user&.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      #本来は読書記録一覧へリダイレクトさせるが、暫定的にユーザー詳細画面へリダイレクト
+      redirect_to @user,notice: "ユーザー「#{@user.name}」がログインしました。"
+    else
+      render :new
+    end
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
