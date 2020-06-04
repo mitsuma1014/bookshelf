7f211e7f7a7f461ed19a09143dbb1ea6b573994a
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-06-05 00:01:05 +0900

    book_authorsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_145432) do
+ActiveRecord::Schema.define(version: 2020_06_04_145933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "book_authors", force: :cascade do |t|
+    t.string "author", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
