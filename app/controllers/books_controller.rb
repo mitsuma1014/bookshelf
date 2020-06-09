2544adf7bd16d1ceb79175544111e7d0c0665818
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-06-09 23:11:22 +0900

    読書記録編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,21 +9,28 @@ class BooksController < ApplicationController
 
   def new
     @book = Book.new
-    @book.book_authors.build
+    3.times{ @book.book_authors.build }
   end
 
   def create
     @book = current_user.books.new(book_params)
     if @book.save
-    redirect_to :books, notice: "本棚に『#{@book.title}』を追加しました。"
+      redirect_to :books, notice: "本棚に『#{@book.title}』を追加しました。"
     else 
       render :new
     end
   end
-
+  
   def edit
+    if @book.book_authors.count == 0
+      3.times{ @book.book_authors.build }  
+    elsif @book.book_authors.count == 1 
+      2.times{ @book.book_authors.build }
+    elsif @book.book_authors.count == 2
+       @book.book_authors.build
+    end
   end
-
+  
   def update
     if @book.update(book_params)
       redirect_to :books, notice: "読書記録を更新しました。"
@@ -31,18 +38,18 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-
+  
   def destroy
     @book.destroy
     redirect_to :books, notice: "本棚から#{@book.title}を削除しました。"
   end
-
+  
   private
-
+  
   def book_params
-    params.require(:book).permit(:title, :genre, :finished_at, :description,book_authors_attributes: [:author])
+    params.require(:book).permit(:title, :genre, :finished_at, :description,book_authors_attributes: [:id,:author])
   end
-
+  
   def set_book
     @book = current_user.books.find(params[:id])
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,16 @@
 class Book < ApplicationRecord
   has_many :book_authors, dependent: :destroy
   belongs_to :user
-  accepts_nested_attributes_for :book_authors
+  accepts_nested_attributes_for :book_authors, reject_if: :reject_book_authors, allow_destroy: true
+
+  def reject_book_authors(attributes)
+    exists = attributes[:id].present?
+    empty = attributes[:author].blank?
+    attributes.merge!(_destroy:1) if exists && empty
+    !exists && empty
+  end
 
   validates :title, presence:true
 
 end
+
