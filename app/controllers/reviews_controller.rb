d93b1e93535b7c773310189d4fed241597b5f73e
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-06-14 17:29:47 +0900

    投稿編集機能を作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,6 +23,23 @@ class ReviewsController < ApplicationController
 
 
   def edit
+    @review = current_user.reviews.find(params[:id])
+    if @review.review_authors.count == 0
+      Review::AUTHORS_FORM.times{ @review.review_authors.build }
+    elsif @review.review_authors.count == 1
+      (Review::AUTHORS_FORM - 1).times{@review.review_authors.build }
+      elsif @review.review_authors.count == 2
+        @review.review_authors.build
+    end
+  end
+
+  def update
+    @review = current_user.reviews.find(params[:id])
+    if @review.update(review_params)
+      redirect_to :reviews, notice: "投稿を更新しました。"
+    else
+      render :edit
+    end
   end
 
   private
